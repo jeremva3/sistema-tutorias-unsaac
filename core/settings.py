0b42b4b84ae36ba68b92e38774a29581e1360f41
jeremva3 <73268196+jeremva3@users.noreply.github.com>
2024-09-19 21:33:22 -0500

    merge

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=*8b(&3*ma$ob^(rl3c^zr(r6q502!w#5ndf9()2j+)6_h+_=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -136,7 +136,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('', include('app.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
